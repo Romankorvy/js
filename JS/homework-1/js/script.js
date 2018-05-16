755e99ea2a16ce010301555e127807e9f0b6cf8d
romankorvy <korvy85@gmail.com>
2018-05-16 16:47:18 +0300

    homework

---
+++ JS/homework-1/js/script.js
@@ -0,0 +1,16 @@
+'use strict';
+// task-1
+var framesPerSec;
+var connectSingOfWords;
+var viewModeDoc;
+var lastPageNav;
+// task-2
+var admin, name;
+ name = "Василий";
+ admin = name;
+ alert(admin);
+// task-3
+var ourPlanetName = "Земля";
+var user = "Петя";
+
+
