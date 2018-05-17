9e02ab02225bc4dfa1d467ba5843d82a4f761bbc
romankorvy <korvy85@gmail.com>
2018-05-17 09:02:48 +0300

    homework

--- public/views/JS/homework-1/js/script.js
+++ public/views/JS/homework-1/js/script.js
@@ -1,14 +1,17 @@
 "use strict";
+
 // task-1
 var framesPerSec;
 var connectSingOfWords;
 var viewModeDoc;
 var lastPageNav;
+
 // task-2
 var admin, name;
- name = "Василий";
- admin = name;
- console.log(admin); 
+name = "Василий";
+admin = name;
+console.log(admin);
+
 // task-3
 var ourPlanetName = "Земля";
 var user = "Петя";
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express'),
+  path = require('path'),
+  app = express(),
+  port = 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(express.static(__dirname + '/public/views'));
+
+// app.set('views', path.join(__dirname, 'public/views'));
+
+app.get('/', (request, response) => {
+  response.sendFile('index.html', { root: __dirname });
+});
+app.get('/:section/lesson-:num', (request, response) => {
+  response.sendFile('index.html', { root: __dirname + '/public/views/' + request.params.section + '/lesson-' + request.params.num });
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('error: ', err);
+  }
+
+  console.log(`server is listening on ${port}`);
+});
