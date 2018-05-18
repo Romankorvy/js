ded902ceb7be9b653a58a834d31695df324af461
romankorvy <korvy85@gmail.com>
2018-05-18 12:46:07 +0300

    homework

--- public/views/JS/homework-1/js/script.js
+++ public/views/JS/homework-1/js/script.js
@@ -1,19 +1,19 @@
-"use strict";
+'use strict';
 
 // task-1
-var framesPerSec;
-var connectSingOfWords;
-var viewModeDoc;
-var lastPageNav;
+var fps; //количество кадров в секунду
+var someValue; //соеденительный знак между словами (дефис)
+var DocViewMode;//режим просмотра документа
+var showLastNavPage;//необходимость показа последней страницы постраничной навигации
 
 // task-2
 var admin, name;
-name = "Василий";
-admin = name;
+name = 'Василий'; // присваиваем переменной name значение "Василий"
+admin = name; // присваиваем переменной admin значение name.
 console.log(admin);
 
 // task-3
-var ourPlanetName = "Земля";
-var user = "Петя";
+var ourPlanetName = 'Земля';// присваиваем переменной ourPlanetName значение "Земля"
+var userName = 'Петя';// присваиваем переменной userName (имя пользователя) значение "Петя"
 
 
